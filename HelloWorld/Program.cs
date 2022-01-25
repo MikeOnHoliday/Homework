dcd4810800588f26e629c12173f28d213dde3803
Mike <michaelbmeyers24@gmail.com>
2022-01-24 21:02:11 -0500

    Hello World :)

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Looking forward to this journey!");
+            Console.WriteLine("Hope I did this homework right");
+        }
+    }
+}
